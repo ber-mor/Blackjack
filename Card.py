93145828db56b4f1bc138fb02ac8751baf66f6d8
Bernardo Morcon <bernardo.morcon@gmail.com>
2019-08-15 01:55:54 -0600

    Initial commit. Card class and deck initialization

---
+++ Card.py
@@ -0,0 +1,27 @@
+class Card:
+	def __init__(self, suit, rank, num_value):
+		self.suit = suit
+		self.rank = rank
+		self.num_value = num_value
+
+deck = []
+suit = ["♥", "♦", "♣", "♠"] 
+rank = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+num_value = [1,2,3,4,5,6,7,8,9,10]
+
+for i in range(0,13):
+	deck.append(Card("♥",rank[i], i+1 if i<10 else 10))
+	deck.append(Card("♦",rank[i], i+1 if i<10 else 10))
+	deck.append(Card("♣",rank[i], i+1 if i<10 else 10))
+	deck.append(Card("♠",rank[i], i+1 if i<10 else 10))
+
+for card in deck:
+	print(card.rank, card.suit, card.num_value)
+
+	
+
+
+
+
+
+
