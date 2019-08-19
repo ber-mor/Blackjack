4fc8c032efc19c31b007bab172d62be3ee55bb29
Bernardo Morcon <bernardo.morcon@gmail.com>
2019-08-19 16:08:56 -0600

    Created Deck and Hand classes

--- Card.py
+++ Card.py
@@ -1,13 +1,32 @@
+class Deck:
+	def __init__(self, num_of_decks):
+		self.deck = []
+		self.discarded_deck = []
+		self.num_of_decks = num_of_decks
+
+	# def init_deck(self):
+
 class Card:
 	def __init__(self, suit, rank, num_value):
 		self.suit = suit
 		self.rank = rank
 		self.num_value = num_value
 
+class Hand:
+	def __init__(self, card1, card2):
+		self.hand = []
+		self.hand.append(card1)
+		self.hand.append(card2)
+		self.total = card1.num_value + card2.num_value
+
+	def hit(self, card):
+		self.hand.append(card)
+		self.total += card.num_value
+
 deck = []
-suit = ["♥", "♦", "♣", "♠"] 
-rank = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-num_value = [1,2,3,4,5,6,7,8,9,10]
+suit = ("♥", "♦", "♣", "♠")
+rank = ("A","2","3","4","5","6","7","8","9","10","J","Q","K")
+num_value = (1,2,3,4,5,6,7,8,9,10)
 
 for s in range(4):
 	for r in range(13):
@@ -15,6 +34,7 @@ for s in range(4):
 
 for card in deck:
 	print(card.rank, card.suit, card.num_value)
+
 
 
 
