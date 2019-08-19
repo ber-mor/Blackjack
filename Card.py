52809814d09ce016fc5991258a2d488a4c78baac
Bernardo Morcon <bernardo.morcon@gmail.com>
2019-08-19 14:44:35 -0600

    Small changes

--- Card.py
+++ Card.py
@@ -9,11 +9,12 @@ suit = ["♥", "♦", "♣", "♠"]
 rank = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 num_value = [1,2,3,4,5,6,7,8,9,10]
 
-for i in range(0,13):
-	deck.append(Card("♥",rank[i], i+1 if i<10 else 10))
-	deck.append(Card("♦",rank[i], i+1 if i<10 else 10))
-	deck.append(Card("♣",rank[i], i+1 if i<10 else 10))
-	deck.append(Card("♠",rank[i], i+1 if i<10 else 10))
+for s in range(4):
+	for r in range(13):
+		deck.append(Card(suit[s], rank[r], r+1 if r<10 else 10))
+
+for card in deck:
+	print(card.rank, card.suit, card.num_value)
 
 
 
